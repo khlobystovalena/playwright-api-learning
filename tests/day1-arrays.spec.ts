f70689b3a30a91667f154a9cfde3da3f270ae563
Elena <khlobystovalena@yandex.ru>
2026-06-23 20:17:55 +0300

    day1: arrays

---
+++ tests/day1-arrays.spec.ts
@@ -0,0 +1,29 @@
+import { test } from "@playwright/test";
+
+test("arrays practice", async () => {
+  const users = [
+    { id: 1, name: "Kate", age: 20 },
+    { id: 2, name: "Elena", age: 30 },
+    { id: 3, name: "John", age: 40 },
+  ];
+
+  //Задание 1
+
+  const result = users.find((user) => user.name === "Elena");
+  console.log(result);
+
+  //Задание 2
+
+  const result2 = users.filter((user) => user.age > 25);
+  console.log(result2);
+
+  //Задание 3
+
+  const result3 = users.map((user) => user.name);
+  console.log(result3);
+
+  //Задание 4
+
+  const result4 = users.map(user => ({ name: user.name }))
+  console.log(result4);
+});
